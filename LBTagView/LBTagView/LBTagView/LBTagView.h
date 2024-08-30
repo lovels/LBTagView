729e30cfdafba794f13dbd884e105e64ba368751
Larry <lovels@users.noreply.github.com>
2024-08-30 15:04:53 +0800

    Update LBTagView

--- LBTagView/LBTagView/LBTagView/LBTagView.h
+++ LBTagView/LBTagView/LBTagView/LBTagView.h
@@ -16,31 +16,31 @@ typedef NS_ENUM(NSInteger,LBTagDirection)
 
 @interface LBTagView : UIView
 
-//is can be moved
+// is can be moved
 @property (nonatomic, assign) BOOL canMove;
 
-//text
+// text
 @property (nonatomic, copy) NSString *text;
 
-//default [UIColor whiteColor]
+// default [UIColor whiteColor]
 @property (nonatomic, strong) UIColor *textColor;
 
-//default is LBTagDirectionLeft
+// default is LBTagDirectionLeft
 @property (nonatomic, assign) LBTagDirection direction;
 
-//default [UIFont systemFontOfSize:12]
+// default [UIFont systemFontOfSize:12]
 @property (nonatomic, strong) UIFont *font;
 
-//default [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5]
+// default [UIColor colorWithRed:0 green:0 blue:0 alpha:0.5]
 @property (nonatomic, strong) UIColor *backgroundColor;
 
-//default [UIColor colorWithRed:1 green:1 blue:1 alpha:0.8]
+// default [UIColor colorWithRed:1 green:1 blue:1 alpha:0.8]
 @property (nonatomic, strong) UIColor *circlrColor;
 
-//default [UIColor colorWithRed:0 green:0 blue:0 alpha:0.7]
+// default [UIColor colorWithRed:0 green:0 blue:0 alpha:0.7]
 @property (nonatomic, strong) UIColor *circlrShadowColor;
 
-//tap action
+// tap action
 @property (nonatomic, strong) UITapGestureRecognizer *tapGestureRecognizer;
 
 @end
